03bfddb4e8395f1ea88bb3a961098974ba37cfdd
Cytmo <2826761681k@gmail.com>
2021-10-07 08:59:45 -0700

    update19

--- benchmark/gzip-1.2.4/script/radamsa_script/script_1_option_d/test.sh
+++ benchmark/gzip-1.2.4/script/radamsa_script/script_1_option_d/test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cnt=0
-crash_cnt=0
 rm result/log_if_behave_properly
 while(( $cnt<=100 )) 
 do
   ${origin} -d < ${rad_files4gzip}/input_1_option_d/sample1_fuzzed${cnt}.gz > ${rad_files4gzip}/input_1_option_d/sample1_fuzzed${cnt}.tst 2>/dev/null
   timeout 2 ${reduced} -d <  ${rad_files4gzip}/input_1_option_d/sample1_fuzzed${cnt}.gz > ${rad_files4gzip}/input_1_option_d/sample1_fuzzed${cnt}.ref 2>/dev/null
-  #echo $? >> /dev/null && exit 1 | echo "crashed"${crash_cnt} >> result/log_reduced_crash_status | let "crash_cnt++"
+  echo $? >> result/timeout
   cmp ${rad_files4gzip}/input_1_option_d/sample1_fuzzed${cnt}.tst ${rad_files4gzip}/input_1_option_dsample1_fuzzed${cnt}.ref >&/dev/null >> result/log_if_behave_properly
   echo -e "\n" >>  result/log_if_behave_properly
   rm ${rad_files4gzip}/input_1_option_d/sample1_fuzzed${cnt}.tst
