ffcb61a084a664dfc8d5fa6cf7926f4cb6c9b510
cytmo <cytmo@qq.com>
2021-11-17 02:53:29 -0800

    add mkdir's afl scripts

--- benchmark/mkdir-5.2.1/script/afl_script/test.sh
+++ benchmark/mkdir-5.2.1/script/afl_script/test.sh
@@ -2,14 +2,35 @@
 cd ../../
 . setenv
 cd script/afl_script/
-rm -r result_${1}${2}
-mkdir result_${1}${2}
-echo current options is $1 $2
-echo availiable options:
-echo  "" d1 "" d1/d2 "-m 123124" d1/d2 "-m" d1/d2 "-m 400" d1 "-m 555" d1 "-m 644" d1 "-m 610" d1 "-m 777" d1 "-p" d1/d2 "-p" d1/d2/d3/d4 "-p" d1 "-p" d1/d2
-
-if($2 == "d1/d2" || $1 == "d1/d2") then
-    afl-fuzz -i ${afl_seed}_d1d2 -o result_${1}${2} ${bins}/afl_bin ${1} 
-else
-    afl-fuzz -i ${afl_seed}_d1 -o result_${1}${2} ${bins}/afl_bin ${1} 
-fi
+para=(d1 "NOT IN USE d1/d2" "NOT IN USE-m 123124 d1/d2" "NOT IN USE -m d1/d2" "-m 400 d1" "-m 555 d1" "-m 644 d1" "-m 610 d1" "-m 777 d1" "-p d1/d2" "-p d1/d2/d3/d4" "-p d1" "-p d1/d2")
+cnt=0
+limit=13
+limit_min=-1
+for paras in ${para[@]}; do
+    if [ $cnt -le $limit ]
+    then
+        echo  ${para[${cnt}]} == ${cnt}
+        let "cnt++"
+    fi
+done
+echo please enter the number that you want to run afl with "(0-12)"
+while :
+do
+   
+    read num
+    if [ $num -lt $limit ] && [ $num -gt $limit_min ]
+    then
+        echo option is
+        cat ${afl_seed}${num}/para
+        rm -r result_${num}
+        mkdir result_${num}
+        mkdir temp${num}
+        cd temp${num}
+        afl-fuzz -i ${afl_seed}${num} -o ../result_${num} ${bins}/afl_bin
+        cd ..
+        rm -rf temp${cnt}
+        break
+    else
+        echo please enter a valid number
+    fi
+done
