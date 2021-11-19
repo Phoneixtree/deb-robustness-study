e0732cf48b9b76961e1b756c3ada87c68941678c
cytmo <cytmo@qq.com>
2021-11-18 22:15:46 -0800

    reorder dirs

--- benchmark/gzip-1.2.4_chisel/script/afl_script/script_3_option_t_expected_1/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../../../
-. setenv
-cd script/afl_script/script_3_option_t_expected_1
-rm -r result
-mkdir result
-afl-fuzz -i ${afl_seed}/input_3_option_t -o result ${bins}/gzip_afl_bin -t @@
--- benchmark/gzip-1.2.4_chisel/script/radamsa_script/script_4_option_t/create_fuzzed_testfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cnt=0
-while(( $cnt<=499 )) 
-do
-  radamsa --seed ${cnt} sample1.gz > ${rad_files4gzip}/input_4_option_t/sample1_fuzzed${cnt}.gz
-  let "cnt++"
-done
-
