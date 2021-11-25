c19d78203045394b3f5f840cc0420695d63b1dc6
Phoneixtree <1246307746@qq.com>
2021-11-25 22:31:09 +0800

    update

---
+++ benchmark/razor/grep-2.19/script/radamsa_script/input_dir_script/test.sh
@@ -0,0 +1,54 @@
+  #/bin/bash
+  cd ../../..
+  . setenv
+  cd script/radamsa_script/input_dir_script
+  
+  debloat=${reduced}.$1
+  
+  cnt=0
+  mkdir result
+  echo "clean" > result/log_reduced
+  echo "clean" > result/log_origin
+  while((${cnt} < 500))
+  do
+  { timeout -k 0.5 0.5 ${origin} "a" -v -H -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt}; } >/dev/null
+  echo $? >> result/log_origin 
+  { timeout -k 0.5 0.5 ${origin} "1" -h -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt}; } >/dev/null
+  echo $? >> result/log_origin 
+  { timeout -k 0.5 0.5 ${origin} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -l ; } >/dev/null
+  echo $? >> result/log_origin 
+  { timeout -k 0.5 0.5 ${origin} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -L ; } >/dev/null
+  echo $? >> result/log_origin  
+  { timeout -k 0.5 0.5 ${origin} "randomtext" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -c ; } >/dev/null
+  echo $? >> result/log_origin 
+  { timeout -k 0.5 0.5 ${origin} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -q ; } >/dev/null
+  echo $? >> result/log_origin 
+  { timeout -k 0.5 0.5 ${origin} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -s ; } >/dev/null
+  echo $? >> result/log_origin 
+
+  { timeout -k 0.5 0.5 ${debloat} "a" -v -H -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced 
+  { timeout -k 0.5 0.5 ${debloat} "1" -h -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced 
+  { timeout -k 0.5 0.5 ${debloat} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -l ; } >/dev/null
+  echo $? >> result/log_reduced 
+  { timeout -k 0.5 0.5 ${debloat} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -L ; } >/dev/null
+  echo $? >> result/log_reduced   
+  { timeout -k 0.5 0.5 ${debloat} "randomtext" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -c ; } >/dev/null
+  echo $? >> result/log_reduced 
+  { timeout -k 0.5 0.5 ${debloat} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -q ; } >/dev/null
+  echo $? >> result/log_reduced 
+  { timeout -k 0.5 0.5 ${debloat} "1" -r ${rad_files4grep}/input_dir_fuzzed/input_dir_${cnt} -s ; } >/dev/null
+  echo $? >> result/log_reduced 
+
+
+
+
+  
+  let "cnt++"
+  done
+  
+  #grep -c "0" result/log_reduced
+  #grep -c "2" result/log_reduced
+  ./result_analysis.sh
+  
---
+++ benchmark/razor/grep-2.19/script/radamsa_script/input_script/result_analysis.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+rm result/result
+origin_miss=`grep -o '1' result/log_origin | wc -l`
+reduced_miss=`grep -o '1' result/log_reduced | wc -l`
+origin_hang=`grep -o '124' result/log_origin | wc -l`
+reduced_hang=`grep -o '124' result/log_reduced | wc -l`
+origin_exc=`grep -o '136' result/log_origin | wc -l`
+reduced_exc=`grep -o '136' result/log_reduced | wc -l`
+origin_beh=`grep -o '0' result/log_origin | wc -l`
+reduced_beh=`grep -o '0' result/log_reduced | wc -l`
+origin_beh=${origin_beh}+${origin_miss}
+reduecd_beh=${reduced_beh}+${reduced_miss}
+
+echo  "origin_bin hang "${origin_hang}" times" >> result/result
+echo -e "reduced_bin hang "${reduced_hang}" times\n" >> result/result
+
+echo  "origin_bin has executed "${origin_exc}" exceptions" >> result/result
+echo -e "reduced_bin has "${reduced_exc}" exceptions\n" >> result/result
+
+echo  "origin_bin successfully executed "${origin_beh}" times" >> result/result
+echo -e "reduced_bin successfully executed "${reduced_beh}" times\n" >> result/result
+
+echo "$(<result/result)"
