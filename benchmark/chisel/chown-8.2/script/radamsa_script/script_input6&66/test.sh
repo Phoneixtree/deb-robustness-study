e6faf8d1b25dc5136dc0734a903ede1132bf8012
cytmo <cytmo@qq.com>
2021-11-27 21:46:24 -0800

    add chisel chown and afl results

---
+++ benchmark/chisel/chown-8.2/script/radamsa_script/script_input6&66/test.sh
@@ -0,0 +1,51 @@
+  #/bin/bash
+  cd ../../..
+  . setenv
+  cd script/radamsa_script/"script_input6&66"
+  
+  debloat=${reduced}.$1
+  
+  cnt=0
+  mkdir result
+  
+  echo "clean" > result/log_reduced
+  echo "clean" > result/log_origin
+  while((${cnt} < 500))
+  do
+  touch file1
+  touch file2
+  ln -s file1 symfile1
+  ln -s file2 symfile2
+  { timeout -k 0.5 0.5 ${origin} < ${rad_files4chown}/input6_fuzzed/input6_${cnt}; } >/dev/null
+  echo $? >> result/log_origin
+  rm -rf file1 file2 symfile1 symfile2
+  
+  touch file1
+  touch file2
+  ln -s file1 symfile1
+  ln -s file2 symfile2
+  { timeout -k 0.5 0.5 ${origin} < ${rad_files4chown}/input66_fuzzed/input66_${cnt}; } >/dev/null
+  echo $? >> result/log_origin
+  rm -rf file1 file2 symfile1 symfile2
+   
+  touch file1
+  touch file2
+  ln -s file1 symfile1
+  ln -s file2 symfile2
+  { timeout -k 0.5 0.5 ${reduced} < ${rad_files4chown}/input6_fuzzed/input6_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced
+  rm -rf file1 file2 symfile1 symfile2
+  
+  touch file1
+  touch file2
+  ln -s file1 symfile1
+  ln -s file2 symfile2
+  { timeout -k 0.5 0.5 ${reduced} < ${rad_files4chown}/input66_fuzzed/input66_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced
+  rm -rf file1 file2 symfile1 symfile2
+  
+  let "cnt++"
+  done
+  
+  ./result_analysis.sh
+  
---
+++ benchmark/chisel/grep-2.1.9/script/radamsa_script/input_script/test.sh
@@ -0,0 +1,38 @@
+  #/bin/bash
+  cd ../../..
+  . setenv
+  cd script/radamsa_script/input_script
+  
+  debloat=${reduced}.$1
+  
+  cnt=0
+  mkdir result
+  echo "clean" > result/log_reduced
+  echo "clean" > result/log_origin
+  while((${cnt} < 500))
+  do
+  { timeout -k 0.5 0.5 ${origin} "ye " ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_origin 
+  { timeout -k 0.5 0.5 ${origin} "cold" ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_origin
+  { timeout -k 0.5 0.5 ${origin} "not exist" ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_origin
+  { timeout -k 0.5 0.5 ${origin} -E '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_origin
+  
+  { timeout -k 0.5 0.5 ${reduced} "ye " ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced
+  { timeout -k 0.5 0.5 ${reduced} "cold" ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced
+  { timeout -k 0.5 0.5 ${reduced} "not exist" ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced
+  { timeout -k 0.5 0.5 ${reduced} -E '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' ${rad_files4grep}/input_fuzzed/input_${cnt}; } >/dev/null
+  echo $? >> result/log_reduced 
+  
+  let "cnt++"
+  done
+  
+  #grep -c "0" result/log_reduced
+  #grep -c "2" result/log_reduced
+  ./result_analysis.sh
+  
