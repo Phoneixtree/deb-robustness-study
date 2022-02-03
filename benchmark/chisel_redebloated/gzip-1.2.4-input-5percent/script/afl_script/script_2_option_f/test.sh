e916c9b498d4f20e308483dfe203c656eb03055f
cytmo <cytmo@qq.com>
2022-02-03 05:38:56 -0800

    add chisel's redebloated gzip,grep&chown

---
+++ benchmark/chisel_redebloated/gzip-1.2.4-input-5percent/script/afl_script/script_2_option_f/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ../../../
+. setenv
+cd script/afl_script/script_2_option_f
+rm -r result
+mkdir result
+afl-fuzz -i ${afl_seed}/input_2_option_f -o result ${bins}/gzip_afl_bin -f
---
+++ benchmark/chisel_redebloated/gzip-1.2.4-input-5percent/script/radamsa_script/script_3_option_t_expected_1/result_analysis.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+rm result/result
+reduced_hang=`grep -o '124' result/log_reduced | wc -l`
+reduced_exc=`grep -o '139' result/log_reduced | wc -l`
+reduced_beh=`grep -o '1' result/log_reduced | wc -l`
+echo -e "reduced_bin hang "${reduced_hang}" times\n" >> result/result
+
+echo -e "reduced_bin has "${reduced_exc}" exceptions\n" >> result/result
+
+echo -e "reduced_bin successfully executed "${reduced_beh}" times\n" >> result/result
+
+echo "$(<result/result)"
+
