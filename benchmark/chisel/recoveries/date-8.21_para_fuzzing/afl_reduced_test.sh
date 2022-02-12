19b0a5023b9f6b886559ebcc6ebbecdc51e6d405
Isabel0715 <1512114230@qq.com>
2022-02-12 19:27:44 +0800

    afl test result of robust reduced date

---
+++ benchmark/chisel/recoveries/date-8.21_para_fuzzing/afl_reduced_test.sh
@@ -0,0 +1,11 @@
+  #!/bin/bash
+
+  home_folder=$(pwd)
+  afl_seed=${home_folder}/input/afl_seed
+  afl_bin=${home_folder}/bins/afl_reduced.out
+  afl_result=${home_folder}/result/afl_result/reduced_result
+  
+  afl-fuzz -d -m 100 -i ${afl_seed}/$1 -o ${afl_result}/result_$1 ${afl_bin} 
+
+
+  
--- benchmark/chisel/recoveries/date-8.21_para_fuzzing/afl_test.sh
+++ benchmark/chisel/recoveries/date-8.21_para_fuzzing/afl_test.sh
@@ -3,7 +3,7 @@
   home_folder=$(pwd)
   afl_seed=${home_folder}/input/afl_seed
   afl_bin=${home_folder}/bins/afl_robust_reduced.out
-  afl_result=${home_folder}/result/afl_result
+  afl_result=${home_folder}/result/afl_result/robust_reduced_result
   
   afl-fuzz -d -m 100 -i ${afl_seed}/$1 -o ${afl_result}/result_$1 ${afl_bin} 
 
