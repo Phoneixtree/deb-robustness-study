1ec9719bd2ea9b9f4d3f4791a7e333a50a0b9048
Isabel0715 <1512114230@qq.com>
2022-02-12 19:36:19 +0800

    robust chisel reduced of mkdir

---
+++ benchmark/chisel/recoveries/mkdir-5.2.1/afl_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+home_folder=$(pwd)
+bin=${home_folder}/bins/afl_robust_reduced.out
+afl_seed=${home_folder}/input/afl_seed/
+afl_result=${home_folder}/result/afl_result
+para=(d1 "NOT IN USE d1/d2" "NOT IN USE-m 123124 d1/d2" "NOT IN USE -m d1/d2" "-m 400 d1" "-m 555 d1" "-m 644 d1" "-m 610 d1" "-m 777 d1" "-p d1/d2" "-p d1/d2/d3/d4" "-p d1" "-p d1/d2")
+cnt=0
+limit=13
+limit_min=-1
+# for paras in ${para[@]}; do
+#     if [ $cnt -lt $limit ]
+#     then
+#         cd ${afl_seed}
+#         mkdir $cnt
+#         cd $cnt
+#         echo ${para[${cnt}]} > test
+#         echo  ${para[${cnt}]} == ${cnt}
+#         let "cnt++"
+#     fi
+# done
+echo please enter the number that you want to run afl with "(0-12)"
+while :
+do
+   
+    read num
+    if [ $num -lt $limit ] && [ $num -gt $limit_min ]
+    then
+        echo option is
+        cat ${afl_seed}${num}/test
+        mkdir temp${num}
+        cd temp${num}
+        afl-fuzz -i ${afl_seed}${num} -o ${afl_result}/result_${num} ${bin}
+        cd ..
+        rm -rf temp${cnt}
+        break
+    else
+        echo please enter a valid number
+    fi
+done
